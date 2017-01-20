caabd02f9e5df7055c340b7ad2117b55e1e0432d
juliantejera <juliantejera@outlook.com>
2017-01-20 08:57:45 -0500

    Added latitude and longitude to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120123738) do
+ActiveRecord::Schema.define(version: 20170120135144) do
 
   create_table "photos", force: :cascade do |t|
     t.integer  "user_id"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20170120123738) do
     t.text     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
