cc0166e64f9d7ef525240b59becc4f24c2f520e1
juliantejera <juliantejera@outlook.com>
2017-04-23 22:27:10 -0400

    Added birthdate to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127124902) do
+ActiveRecord::Schema.define(version: 20170424021148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170127124902) do
     t.datetime "updated_at",                               null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.date     "birthdate"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["latitude", "longitude"], name: "index_users_on_latitude_and_longitude", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
