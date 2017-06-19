a7269ba5d08dd3950e1752b6d36892ae165b4731
juliantejera <juliantejera@outlook.com>
2017-06-19 00:08:55 -0400

    Improved file formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 class Coordinate
   attr_accessor :name, :latitude, :longitude
 
@@ -25,7 +17,6 @@ class Coordinate
     )
   end
 end
-
 
 if Rails.env == "development"
   coordinates = [
@@ -47,7 +38,3 @@ if Rails.env == "development"
   ]
   coordinates.each { |coordinate| coordinate.create_user }
 end
-
-
-
-
