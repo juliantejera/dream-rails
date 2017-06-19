55a3e98044b3eaa8ba533a703a682deaf76375b5
juliantejera <juliantejera@outlook.com>
2017-06-19 00:13:27 -0400

    Fixed typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ end
 if Rails.env == "development"
   coordinates = [
     Coordinate.new("home", 40.8235016, -73.9458406),
-    Coordinate.new("time_square", 40.758895, -73.9873197),
+    Coordinate.new("times_square", 40.758895, -73.9873197),
     Coordinate.new("empire_state", 40.7484404, -73.9878441),
     Coordinate.new("penn_station", 40.750568, -73.9957077),
     Coordinate.new("world_trade_center", 40.711556, -74.0145544),
@@ -30,7 +30,7 @@ if Rails.env == "development"
     Coordinate.new("eiffel_tower", 48.8583701, 2.2922926),
     Coordinate.new("louvre_museum", 48.8606111,2.3354553),
     Coordinate.new("great_pyramid_at_giza", 29.9792345, 31.1320132),
-    Coordinate.new("machupichhu", -13.157051, -72.521782),
+    Coordinate.new("machupicchu", -13.157051, -72.521782),
     Coordinate.new("panama_canal", 9.1079631,-79.8641385),
     Coordinate.new("yosemite", 37.8529772,-119.8312937),
     Coordinate.new("yellowstone", 44.5844249,-111.0744731),
